4419c30b1674d453effcca390e5575f5cd1dec7c
Denis Molony <dmolony@users.noreply.github.com>
2022-04-24 16:33:24 +1000

    added specials to MessagePane

--- src/com/bytezone/wizardry/gui/MessagePane.java
+++ src/com/bytezone/wizardry/gui/MessagePane.java
@@ -1,7 +1,10 @@
 package com.bytezone.wizardry.gui;
 
+import java.util.List;
+
 import com.bytezone.wizardry.origin.Location;
 import com.bytezone.wizardry.origin.Message;
+import com.bytezone.wizardry.origin.Special;
 import com.bytezone.wizardry.origin.WizardryOrigin;
 
 import javafx.geometry.HPos;
@@ -15,8 +18,10 @@ public class MessagePane extends DataPane
 // -----------------------------------------------------------------------------------//
 {
   private WizardryOrigin wizardry;
+
   TextField[] textOut1;
   TextField[] textOut2;
+  TextField[] textOut4;
   TextArea textOut3;
 
   // ---------------------------------------------------------------------------------//
@@ -26,7 +31,7 @@ public class MessagePane extends DataPane
     setColumnConstraints (110, 120, 300);
 
     // make all rows the same height
-    RowConstraints rowCo = new RowConstraints (25);
+    RowConstraints rowCo = new RowConstraints (30);
     for (int i = 0; i < 20; i++)
       gridPane.getRowConstraints ().add (rowCo);
 
@@ -35,14 +40,16 @@ public class MessagePane extends DataPane
     DataPlacement dp1 = new DataPlacement (1, 1, Pos.CENTER_LEFT, 1);
     textOut1 = createTextFields (label1Text, lp1, dp1);
 
-    String[] label2Text = { "Locations" };
+    String[] label2Text = { "Location 1", "Location 2", "Location 3" };
     LabelPlacement lp2 = new LabelPlacement (0, 2, HPos.RIGHT, 1);
-    DataPlacement dp2 = new DataPlacement (1, 2, Pos.CENTER_LEFT, 2);
+    DataPlacement dp2 = new DataPlacement (1, 2, Pos.CENTER_LEFT, 1);
     textOut2 = createTextFields (label2Text, lp2, dp2);
 
+    textOut4 = createTextFields (3, new DataPlacement (2, 2, Pos.CENTER_LEFT, 1));
+
     String[] label3Text = { "Text" };
-    LabelPlacement lp3 = new LabelPlacement (0, 3, HPos.RIGHT, 1);
-    DataPlacement2 dp3 = new DataPlacement2 (1, 3, Pos.CENTER_LEFT, 2, 18);
+    LabelPlacement lp3 = new LabelPlacement (0, 5, HPos.RIGHT, 1);
+    DataPlacement2 dp3 = new DataPlacement2 (1, 6, Pos.CENTER_LEFT, 2, 20);
     textOut3 = createTextArea (label3Text, lp3, dp3);
   }
 
@@ -57,18 +64,38 @@ public class MessagePane extends DataPane
   public void update (Message message)
   // ---------------------------------------------------------------------------------//
   {
-    setText (textOut1[0], message.getTotalLocations ());
+    for (TextField textField : textOut2)
+      textField.setText ("");
+    for (TextField textField : textOut4)
+      textField.setText ("");
 
-    StringBuilder text = new StringBuilder ();
-    for (Location location : message.getLocations ())
-      text.append (location + ", ");
-    if (text.length () > 0)
+    List<Location> locations = message.getLocations ();
+
+    setText (textOut1[0], locations.size ());
+
+    if (locations.size () > 0)
     {
-      text.deleteCharAt (text.length () - 1);
-      text.deleteCharAt (text.length () - 1);
+      setText (textOut2[0], locations.get (0));
+      Special special = wizardry.getSpecial (locations.get (0));
+      if (special != null)
+        setText (textOut4[0], special.getText ());
     }
 
-    setText (textOut2[0], text.toString ());
+    if (locations.size () > 1)
+    {
+      setText (textOut2[1], locations.get (1));
+      Special special = wizardry.getSpecial (locations.get (1));
+      if (special != null)
+        setText (textOut4[1], special.getText ());
+    }
+
+    if (locations.size () > 2)
+    {
+      setText (textOut2[2], locations.get (2));
+      Special special = wizardry.getSpecial (locations.get (2));
+      if (special != null)
+        setText (textOut4[2], special.getText ());
+    }
 
     textOut3.setText (message.getText ());
   }
