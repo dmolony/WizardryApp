9a66379af9cf3cd5a0711b501ff2e07514804094
Denis Molony <dmolony@users.noreply.github.com>
2022-11-27 11:10:00 +1000

    updated messages pane

---
+++ src/com/bytezone/wizardry/MessagesPane.java
@@ -0,0 +1,85 @@
+package com.bytezone.wizardry;
+
+import java.util.List;
+
+import com.bytezone.appbase.BorderedDataPane;
+import com.bytezone.appbase.DataLayout;
+import com.bytezone.wizardry.data.Location;
+import com.bytezone.wizardry.data.Message;
+import com.bytezone.wizardry.data.Special;
+import com.bytezone.wizardry.data.WizardryData;
+
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+// -----------------------------------------------------------------------------------//
+public class MessagesPane extends BorderedDataPane
+// -----------------------------------------------------------------------------------//
+{
+  TextField[] textOut1;
+  TextField[] textOut2;
+  TextField[] textOut3;
+
+  TextArea textArea;
+
+  WizardryData wizardry;
+
+  // ---------------------------------------------------------------------------------//
+  public MessagesPane ()
+  // ---------------------------------------------------------------------------------//
+  {
+    super (3, 25);                              // columns, rows
+
+    setColumnConstraints (110, 120, 310);
+
+    String[] labels = { "Used", "Location 1", "Location 2", "Location 3", "Text" };
+    createLabelsVertical (labels, 0, 0, HPos.RIGHT);
+
+    textOut1 = createTextFields (new DataLayout (1, 0, 1, Pos.CENTER_LEFT));
+    DataLayout dataLayout = new DataLayout (1, 1, 3, Pos.CENTER_LEFT);
+    textOut2 = createTextFields (dataLayout);
+    textOut3 = createTextFields (dataLayout);
+
+    textArea = createTextArea (new DataLayout (1, 4, 21, Pos.CENTER_LEFT, 2));
+  }
+
+  // ---------------------------------------------------------------------------------//
+  public void setWizardry (WizardryData wizardry)
+  // ---------------------------------------------------------------------------------//
+  {
+    this.wizardry = wizardry;
+
+    reset (textOut1);
+    reset (textOut2);
+    reset (textOut3);
+
+    textArea.setText ("");
+  }
+
+  // ---------------------------------------------------------------------------------//
+  void update (Message message)
+  // ---------------------------------------------------------------------------------//
+  {
+    for (TextField textField : textOut2)
+      textField.setText ("");
+    for (TextField textField : textOut3)
+      textField.setText ("");
+
+    List<Location> locations = message.getLocations ();
+
+    setText (textOut1[0], locations.size ());
+
+    for (int i = 0; i < 3; i++)
+      if (locations.size () > i)
+      {
+        setText (textOut2[i], locations.get (i));
+        Special special = wizardry.getSpecial (locations.get (i));
+        if (special != null)
+          setText (textOut3[i], special.getText ());
+      }
+
+    textArea.setText (message.getText ());
+  }
+}
--- src/com/bytezone/wizardry/MessagesRootPane.java
+++ src/com/bytezone/wizardry/MessagesRootPane.java
@@ -1,30 +1,14 @@
 package com.bytezone.wizardry;
 
-import java.util.List;
-
-import com.bytezone.appbase.DataLayout;
 import com.bytezone.appbase.DataPane;
-import com.bytezone.wizardry.data.Location;
 import com.bytezone.wizardry.data.Message;
-import com.bytezone.wizardry.data.Special;
 import com.bytezone.wizardry.data.WizardryData;
-
-import javafx.geometry.HPos;
-import javafx.geometry.Pos;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 
 // -----------------------------------------------------------------------------------//
 public class MessagesRootPane extends DataPane
 // -----------------------------------------------------------------------------------//
 {
-  private WizardryData wizardry;
-
-  TextField[] textOut1;
-  TextField[] textOut2;
-  TextField[] textOut3;
-
-  TextArea textArea;
+  MessagesPane messagesPane = new MessagesPane ();
 
   // ---------------------------------------------------------------------------------//
   public MessagesRootPane ()
@@ -32,56 +16,25 @@ public class MessagesRootPane extends DataPane
   {
     super (3, 25);                             // columns, rows
 
-    setColumnConstraints (110, 120, 310);
-    setPadding (defaultInsets);
-    //    setGridLinesVisible (true);
+    setAllColumnConstraints (10);             // all columns 10 pixels wide
+    setPadding (defaultInsets);               // only the root pane has insets
 
-    String[] labels = { "Used", "Location 1", "Location 2", "Location 3", "Text" };
-    createLabelsVertical (labels, 0, 0, HPos.RIGHT);
+    setLayout (messagesPane, 0, 0);
 
-    textOut1 = createTextFields (new DataLayout (1, 0, 1, Pos.CENTER_LEFT));
-    DataLayout dataLayout = new DataLayout (1, 1, 3, Pos.CENTER_LEFT);
-    textOut2 = createTextFields (dataLayout);
-    textOut3 = createTextFields (dataLayout);
-
-    textArea = createTextArea (new DataLayout (1, 4, 21, Pos.CENTER_LEFT, 2));
+    getChildren ().addAll (messagesPane);
   }
 
   // ---------------------------------------------------------------------------------//
   public void setWizardry (WizardryData wizardry)
   // ---------------------------------------------------------------------------------//
   {
-    this.wizardry = wizardry;
-
-    reset (textOut1);
-    reset (textOut2);
-    reset (textOut3);
-
-    textArea.setText ("");
+    messagesPane.setWizardry (wizardry);
   }
 
   // ---------------------------------------------------------------------------------//
   public void update (Message message)
   // ---------------------------------------------------------------------------------//
   {
-    for (TextField textField : textOut2)
-      textField.setText ("");
-    for (TextField textField : textOut3)
-      textField.setText ("");
-
-    List<Location> locations = message.getLocations ();
-
-    setText (textOut1[0], locations.size ());
-
-    for (int i = 0; i < 3; i++)
-      if (locations.size () > i)
-      {
-        setText (textOut2[i], locations.get (i));
-        Special special = wizardry.getSpecial (locations.get (i));
-        if (special != null)
-          setText (textOut3[i], special.getText ());
-      }
-
-    textArea.setText (message.getText ());
+    messagesPane.update (message);
   }
 }
