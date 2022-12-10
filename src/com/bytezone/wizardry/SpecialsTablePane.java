d98f7920a60962a5b8757d6d7888a8509393240d
Denis Molony <dmolony@users.noreply.github.com>
2022-12-10 21:52:11 +1000

    adjusted column widths

--- src/com/bytezone/wizardry/SpecialsTablePane.java
+++ src/com/bytezone/wizardry/SpecialsTablePane.java
@@ -24,7 +24,7 @@ public class SpecialsTablePane extends BorderedDataPane
     super (9, 17);                              // columns, rows
 
     int width = 63;
-    setColumnConstraints (110, 120, width, width, width, width, 80, 40, 320);
+    setColumnConstraints (30, 85, width, width, width, width, 80, 40, 320);
 
     String[] headings = { "Square type", "Aux 0", "Aux 1", "Aux 2", "Occurs", "First location",
         "Msg #", "Description" };
